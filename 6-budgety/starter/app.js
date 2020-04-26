b5532cbf66e870acbc41d607cef0d433449fe240
Nisha <nishathapa4727@gmail.com>
2020-04-27 01:46:45 +0530

    Displaying The Current Month

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -189,7 +189,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -308,6 +309,17 @@ var UIController = (function() {
             });
 
         },
+        displayMonth: function() {
+            var now,month,months, year;
+
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
 
        
         getDOMstrings: function(){
@@ -417,6 +429,7 @@ var setupEventListeners = function(){
     return {
         init: function(){
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
